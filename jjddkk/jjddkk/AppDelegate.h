21300a9e10f7ce9dd46422fce0b0003207caae0c
Shawn Shen <shawnshen@shawndemacbook-air.local>
2016-10-13 03:50:15 +0800

    是这样的

---
+++ jjddkk/jjddkk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jjddkk
+//
+//  Created by Shawn Shen on 2016/10/13.
+//  Copyright © 2016年 Shawn Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jjddkk/jjddkk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jjddkk
+//
+//  Created by Shawn Shen on 2016/10/13.
+//  Copyright © 2016年 Shawn Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
